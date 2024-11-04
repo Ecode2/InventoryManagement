80336c9bf6dce3fb9fafe257cacf3a28e9440f90
Abubakar Alaya <ecode5814@gmail.com>
2024-11-04 09:04:05 +0000

    made changes to inventory

--- src/system/helpers/db.py
+++ src/system/helpers/db.py
@@ -1,14 +1,13 @@
-from .env import BASE_DIR, config
-from urllib.parse import urlparse
+import dj_database_url
 from pathlib import Path
 from functools import lru_cache
 
-import dj_database_url
+from .env import BASE_DIR, config
 
 @lru_cache
 def get_database_url(BASE_DIR:Path, DEBUG:bool, DATABASE_URL)->dict:
     if DEBUG:
-        return {
+        """ return {
                 'default': {
                     'ENGINE': 'django.db.backends.sqlite3',
                     'NAME': BASE_DIR.parent / 'db.sqlite3',
@@ -16,15 +15,16 @@ def get_database_url(BASE_DIR:Path, DEBUG:bool, DATABASE_URL)->dict:
                     'timeout': 20,  # 20 seconds timeout
                     }
                 }
-            }
-        """ return {
-            'default': dj_database_url.config(default="postgres://default:HePa1sdgW7vM@ep-young-smoke-a4l644r7-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require",
+            } """
+        
+        return {
+            'default': dj_database_url.config(default=DATABASE_URL,
                                           conn_max_age=600,
                                           ssl_require=True,
                                           conn_health_checks=True)
-        } """
-        
-        
+        }
+
+
     return {
         'default': dj_database_url.config(default=DATABASE_URL,
                                           conn_max_age=600,
