243c1672f4b5ed3ca4a43e016e64c433a29515bf
Ecode2 <ecode5814@gmail.com>
2024-11-08 09:20:19 +0100

    implemented delete feature

--- src/static/js/dashboard/sales.js
+++ src/static/js/dashboard/sales.js
@@ -253,10 +253,16 @@ const sales = async (role, page) => {
                       </dl>
                   </div>
                   <!-- Modal footer -->
-                  <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+                  <div class="flex items-center w-full justify-evenly p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
                       <button type="button" onclick="download_receipt(${receipt_id})"
-                      class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">Receipt</button>
+                          class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">
+                          Receipt
+                      </button>
                       <button data-modal-hide="${sale.sale_uuid.substring(0, 8)}-detail-modal" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Close</button>
+                      <button type="button" onclick="deleteCart(${sale.id})"
+                          class="rounded-lg border border-red-700 py-2.5 px-5 ms-3 text-center text-sm font-medium text-red-700 hover:bg-red-700 hover:text-white focus:outline-none focus:ring-4 focus:ring-red-300 dark:border-red-500 dark:text-red-500 dark:hover:bg-red-600 dark:hover:text-white dark:focus:ring-red-900">
+                          Delete
+                      </button>
                   </div>
               </div>
           </div>
@@ -361,7 +367,7 @@ const sales = async (role, page) => {
                                 class="w-full rounded-lg  border border-gray-200 bg-white px-5  py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700">
                                 Close
                               </button>
-                              <button type="button" onclick="deleteCart(${sale.id})"
+                              <button type="button" onclick="calcelCart(${sale.id})"
                                 class="w-full rounded-md border border-red-700 px-3 py-2 text-center text-sm font-medium text-red-700 hover:bg-red-700 hover:text-white focus:outline-none focus:ring-4 focus:ring-red-300 dark:border-red-500 dark:text-red-500 dark:hover:bg-red-600 dark:hover:text-white dark:focus:ring-red-900 md:w-auto">
                                 Cancel
                               </button>
@@ -570,7 +576,7 @@ const createCart = async (warehouse_id) => {
             Close
           </button>
           
-          <button type="button" onclick="deleteCart(${data.id})"
+          <button type="button" onclick="calcelCart(${data.id})"
             class="w-full rounded-md border border-red-700 px-3 py-2 text-center text-sm font-medium text-red-700 hover:bg-red-700 hover:text-white focus:outline-none focus:ring-4 focus:ring-red-300 dark:border-red-500 dark:text-red-500 dark:hover:bg-red-600 dark:hover:text-white dark:focus:ring-red-900 md:w-auto">
             Cancel
           </button>
@@ -582,18 +588,10 @@ const createCart = async (warehouse_id) => {
       </div>`
 };
 
-const deleteCart = async (cart_id) => {
+const calcelCart = async (cart_id) => {
   popup = confirm("Delete Current Cart ?")
-  console.log(popup)
   if (popup) {
       try {
-          /* const new_cart = await fetch(`/api/sale/cart/${cart_id}/`, {
-            method: 'DELETE',
-            headers: {
-              'Content-Type': 'application/json',
-              'X-CSRFToken': getCookie('csrftoken')
-            }
-          }) */
           const new_cart = await fetch(`/api/sale/cart/${cart_id}/`, {
             method: "PATCH",
             headers: {
@@ -610,13 +608,42 @@ const deleteCart = async (cart_id) => {
             return;
           }
           window.location.reload();
-          DisplayMessage("Cart deleted", "info");
+          DisplayMessage("Sale canceled", "info");
 
       } catch (error) {
         DisplayMessage("Couldn't delete. Try again", "error");
       }
   }
 };
+
+const deleteCart = async (cart_id) => {
+  popup = confirm("Delete Current Cart ?")
+  console.log(popup)
+  if (popup) {
+      try {
+          const new_cart = await fetch(`/api/sale/cart/${cart_id}/`, {
+            method: 'DELETE',
+            headers: {
+              'Content-Type': 'application/json',
+              'X-CSRFToken': getCookie('csrftoken')
+            }
+          })
+
+          if (!new_cart.ok) {
+            console.log(new_cart)
+            DisplayMessage("Couldn't delete cart. Try again", "error");
+            return;
+          }
+          window.location.reload();
+          DisplayMessage("Cart Deleted", "info");
+
+      } catch (error) {
+        console.log(error)
+        DisplayMessage("Couldn't delete. Try again", "error");
+      }
+  }
+}
+
 
 const finalizeSale = async (cart_id, payment_id) => {
   const payment_method = document.getElementById(payment_id);
@@ -1006,21 +1033,21 @@ async function SalesPage(role, warehouse_id) {
             <div class="mx-auto max-w-screen-xl px-4 2xl:px-0">
                 <div id="customer_info_container" class="space-y-4 border-b border-gray-200 py-8 dark:border-gray-700">
                     <h4 class="text-lg font-semibold text-gray-900 dark:text-white">Customer Information</h4>
-                    <div class="w-full flex justify-between">
-                      <div class="flex">
-                        <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-e-0 border-gray-300 rounded-s-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
-                          <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                            <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"/>
-                          </svg>
-                        </span>
-                        <input type="text" id="customer_cart" class="rounded-none rounded-e-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-                        placeholder="Customer Name">
-                      </div>
-                      <button type="submit" onclick="createCart('${warehouse_id}')"
-                        class="text-white bg-primary-700 rounded-lg hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 text-sm w-full sm:w-auto px-5 py-2.5 text-center ">
-                        Submit
-                      </button>
-                    </div>
+                    <form id="" onsubmit="handleSubmit(event, createCart, ${warehouse_id})" class="w-full flex justify-between">
+                        <div class="flex">
+                          <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-e-0 border-gray-300 rounded-s-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+                            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                              <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"/>
+                            </svg>
+                          </span>
+                          <input type="text" id="customer_cart" class="rounded-none rounded-e-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                          placeholder="Customer Name">
+                        </div>
+                        <button type="submit" onclick="createCart('${warehouse_id}')"
+                          class="text-white bg-primary-700 rounded-lg hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 text-sm w-full sm:w-auto px-5 py-2.5 text-center ">
+                          Submit
+                        </button>
+                    </form>
                 </div>
 
                 <div id="cart_info" class="py-2">
